e600b16466f38052fd900870aea300cf40e98c56
haoyunyun888 <13126726903@163.com>
2019-06-06 17:00:12 +0800

    测试版

---
+++ src/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: M
+ * Date: 2019/6/6
+ * Time: 16:55
+ */
+
+namespace magi\magiclor;
+
+
+class Db
+{
+    public function test()
+    {
+        echo "hello world";
+    }
+}
